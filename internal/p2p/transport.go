e6a51ffd29fe2ded50f358d474449346d1e29f9f
Jamie Hands <jamie_hands0@protonmail.com>
2025-06-18 12:00:20 +0100

    docs: added comments for the interfaces in p2p

--- internal/p2p/transport.go
+++ internal/p2p/transport.go
@@ -1,7 +1,9 @@
 package p2p
 
+// Peer represents a remote node over any type of connection
 type Peer interface{}
 
+// Transport represents any type of connection e.g. TCP, UDP, websockets, gRPC
 type Transport interface {
 	ListenAndAccept() error
 }
