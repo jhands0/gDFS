252e9a1a65ca08aae5a923817e17e5450ccedbd5
Jamie Hands <jamie_hands0@protonmail.com>
2025-06-19 16:19:40 +0100

    feat: added DefaultDecoder struct for tests and partially implemented the Decode() func

--- internal/p2p/encoding.go
+++ internal/p2p/encoding.go
@@ -5,3 +5,14 @@ import "io"
 type Decoder interface {
 	Decode(io.Reader, any) error
 }
+
+type DefaultDecoder struct{}
+
+func (dec DefaultDecoder) Decode(r io.Reader, msg any) error {
+	peekBuf := make([]byte, 1)
+	if _, err := r.Read(peekBuf); err != nil {
+		return nil
+	}
+
+	return nil
+}
