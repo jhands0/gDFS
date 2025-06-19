24486f0a6af9a056fa3ffba2cb52e75db8232a72
Jamie Hands <jamie_hands0@protonmail.com>
2025-06-19 16:31:11 +0100

    test: added TCPTransportOptions struct to tests

--- internal/p2p/tcp_transport_test.go
+++ internal/p2p/tcp_transport_test.go
@@ -7,9 +7,14 @@ import (
 )
 
 func TestTCPTransport(t *testing.T) {
+	tcpOption := TCPTransportOptions{
+		ListenAddress: ":4000",
+		ShakeHands:    NOPHandshake,
+		Decoder:       DefaultDecoder{},
+	}
 	listenAddr := ":4000"
-	tr := NewTCPTransport(listenAddr)
-	assert.Equal(t, tr.listenAddress, listenAddr)
+	tr := NewTCPTransport(tcpOption)
+	assert.Equal(t, tr.ListenAddress, listenAddr)
 
 	assert.Nil(t, tr.ListenAndAccept())
 }
